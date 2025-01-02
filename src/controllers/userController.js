0e6d05bcbfc595475a05896984b94befc3c20b5f
sekhar <sekharchaudhary5@gmail.com>
2025-01-02 15:42:44 +0530

    feat: delete user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -41,9 +41,22 @@ const updateUser = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 }
+const userDelete = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const deletedUser = await User.findByIdAndDelete(id);
+        if (!deletedUser) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.status(200).json({ message: 'User deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
 module.exports = {
     getAllUsers,
     createUser,
     getUserDetails,
-    updateUser
+    updateUser,
+    userDelete
 };
--- src/routes/Userrouter.js
+++ src/routes/Userrouter.js
@@ -6,5 +6,6 @@ router.get('/', userController.getAllUsers);
 router.get('/:id', userController.getUserDetails);
 router.post('/', userController.createUser);
 router.put('/:id', userController.updateUser);
+router.delete('/:id', userController.userDelete);
 
 module.exports = router;
